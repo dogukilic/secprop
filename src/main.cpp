288d4ff5fe9dc1814f5373b270ea429aac691a8c
Dogukan Kilic <dogukankilic96@gmail.com>
2026-04-28 22:33:13 +0300

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 
 int main() {
 
-    //std::cout << "Point2D class test" << std::endl;
-    //Point2D::Test();
-
     std::cout << "Vector class test" << std::endl;
     
     Vector vec(5);
     vec.Print();
+
 
     return 0;
 }
